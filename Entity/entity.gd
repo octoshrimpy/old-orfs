3df18390e4131fac018056731b5c6a579c3d6de3
Rocco Nicholls <rocco11nicholls@gmail.com>
2021-03-29 08:55:07 -0600

    Fixed entity flip issue

--- Entity/entity.gd
+++ Entity/entity.gd
@@ -4,7 +4,7 @@ class_name Entity
 
 var what_do = {}
 
-
+var sprite_faces_right = true
 var speed = 150
 var path : = PoolVector2Array()
 
@@ -41,7 +41,6 @@ func _process(delta):
     
 # called every _process
 func _do_pathing(delta):
-  
   #drop out early
   if not path.size() > 0: 
     return
@@ -49,17 +48,17 @@ func _do_pathing(delta):
   # Calculate the movement distance for this frame
   var distance_to_walk = speed * delta
 
-  # Move the player along the path until he has run out of movement or the path ends.
+  # Move the entity along the path until he has run out of movement or the path ends.
   while distance_to_walk > 0 and path.size() > 0:
     var distance_to_next_point = position.distance_to(path[0])
     if distance_to_walk <= distance_to_next_point:
       position += position.direction_to(path[0]) * distance_to_walk
       if position.direction_to(path[0]).x > 0:
-        $Sprite.flip_h = false
+        $Sprite.flip_h = !sprite_faces_right
       else:
-        $Sprite.flip_h = true
+        $Sprite.flip_h = sprite_faces_right
     else:
-      # The player get to the next point
+      # The entity get to the next point
       position = path[0]
       path.remove(0)
     # Update the distance to walk
@@ -77,7 +76,7 @@ func _idle(delta):
   _idle_timer = Timer.new()
   add_child(_idle_timer)
   
-  #BUG timer isn't changing time, and wandering around is same time between calls
+  # BUG timer isn't changing time, and wandering around is same time between calls
   _idle_timer.connect("timeout", self, "_on_idle_timer_timeout")
   self._rand_timer_count = rand_range(1, 3)
   print(self._rand_timer_count)
